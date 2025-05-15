fced33831f2aa6259e6ee67925e00111869eb9a6
santigiac <452598.joan23@fje.edu>
2025-05-15 10:17:38 +0200

    forms

--- index.php
+++ index.php
@@ -11,10 +11,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página principal</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <title>🐾Perriatra🐾</title>
+
 </head>
 <body>
-    
+<!-- Saludar al usuario logeado y con posibilidad de cerrar sesión -->
+    <header> 
+        <h1>Festival Internacional de Música</h1>
+        <p>Bienvenido, <?php echo ($_SESSION['username']); ?>! 
+            <a href="proces/logout.php">Cerrar sesión</a>
+        </p>
+    </header>
     
 </body>
 </html>
